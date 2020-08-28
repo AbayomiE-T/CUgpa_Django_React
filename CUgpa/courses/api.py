64b58b88d20f5b36e588b9ddb3dddfe47b0c7a7a
Abayomi <eyitouyoa3@gmail.com>
2020-08-28 08:54:06 -0400

    created accounts app with django-knox-authentication implemented

--- CUgpa/courses/api.py
+++ CUgpa/courses/api.py
@@ -2,11 +2,27 @@ from .models import Course
 from rest_framework import viewsets, permissions
 from .serializers import CourseSerializer
 
-class CourseViewSet(viewsets.ModelViewSet):
 
-    queryset = Course.objects.all()
+class CourseViewSet(viewsets.ModelViewSet):
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
 
     serializer_class = CourseSerializer
+
+    def get_queryset(self):
+        return self.request.user.courses.all()
+
+    # Allows us to save the owner associated with a course when a course is created
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    # Use this code before creating an accounts app to be
+    # able to see all courses without authentication.
+
+    # queryset = Course.objects.all()
+    # permission_classes = [
+    #     permissions.AllowAny
+    # ]
+
+    # serializer_class = CourseSerializer
--- CUgpa/courses/models.py
+++ CUgpa/courses/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-#After creating a model, type in the following commands into the command line:
+# After creating a model, type in the following commands into the command line:
 
-#python manage.py makemigrations
-#python manage.py migrate 
+# python manage.py makemigrations
+# python manage.py migrate
+
+
 class Course(models.Model):
-      
-      courseName = models.CharField(max_length=100)
-      courseCode = models.CharField(max_length=20)
-      creditValue = models.FloatField()
-      grade = models.CharField(max_length=3)
-      gradePoint = models.FloatField()
+
+    courseName = models.CharField(max_length=100)
+    courseCode = models.CharField(max_length=20)
+    creditValue = models.FloatField()
+    grade = models.CharField(max_length=3)
+    gradePoint = models.FloatField()
+    owner = models.ForeignKey(
+        User, related_name="courses", on_delete=models.CASCADE, null=True)
